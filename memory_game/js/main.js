c5b6671c41eae8d0195c68cb0e133fe80a329336
Annette C <29418594+blankol@users.noreply.github.com>
2017-11-22 18:35:41 -0500

    Update main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,32 +11,30 @@ var cardsInPlay = [cardFour]*/
 //var cardFour = cards [3];
 
 
-//cardsInPlay.push("cardOne");
-//console.log("User flipped " + cardOne);
+//cardsInPlay.push("cardOne")
+//console.log("User flipped " + cardOne)
 
-//cardsInPlay.push("cardTwo");
-//console.log("User flipped " + cardTwo);
+//cardsInPlay.push("cardTwo")
+//console.log("User flipped " + cardTwo)
 
 
 var cardsInPlay = []
-
 // if (cardsInPlay.length === 2)
-    { if (cardsInPlay[0] === cardsInPlay [1]) {
-    alert("You found a match!")} 
-    else { 
+{ if (cardsInPlay[0] === cardsInPlay [1]) {
+        alert("You found a match!")
+      } else { 
         alert("Sorry, try again!")
-    };
+    }
 }
 
 var flipCard = function (cardId) {
-    console.log ("User flipped" + cards[cardId]); //check again
-    cardsInPlay.push("cards[cardId]");
+        console.log ("User flipped" + cards[cardId]); //check again
+        cardsInPlay.push("cards[cardId]")
 }
 
 function checkForMatch () {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-        } else {
-        alert("Sorry, try again!");
-        }
-}
+        alert("You found a match!")
+      } else { 
+        alert("Sorry, try again!")
+    }
